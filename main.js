99df7bf394a62e3651ff5d2684f5b850fd32a78c
Yash Negi <yashnegi36442@gmail.com>
2024-06-21 19:47:16 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/* Menu show */
+/* Menu show */ 
 if(navToggle) {
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
@@ -14,43 +14,5 @@ if(navToggle) {
 if(navClose) {
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
-    })
-}
-
-/*=============== SEARCH ===============*/
-const search = document.getElementById('search'),
-      searchBtn = document.getElementById('search-btn'),
-      searchClose = document.getElementById('search-close')
-
-/* Search show */
-if(searchBtn) {
-    searchBtn.addEventListener('click', () =>{
-        search.classList.add('show-search')
-    })
-}
-
-/* Search hidden */
-if(searchClose) {
-    searchClose.addEventListener('click', () =>{
-        search.classList.remove('show-search')
-    })
-}
-
-/*=============== LOGIN ===============*/
-const login = document.getElementById('login'),
-      loginBtn = document.getElementById('login-btn'),
-      loginClose = document.getElementById('login-close')
-
-/* Login show */
-if(loginBtn) {
-    loginBtn.addEventListener('click', () =>{
-        login.classList.add('show-login')
-    })
-}
-
-/* Login hidden */
-if(loginClose) {
-    loginClose.addEventListener('click', () =>{
-        login.classList.remove('show-login')
     })
 }
